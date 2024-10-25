b0c444c20b9c300ad30a3dea0c9e435a68f5b456
mnethaji838 <mnethaji838@gmail.com>
2024-10-25 14:46:55 +0530

    project-complete

---
+++ third-project/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { Main } from './Main';
+import { Footer } from './Footer';
+import { Head } from './Head';
+
+function App() {
+  return (
+    <div className="App">
+      <Head/>
+      <Main/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
