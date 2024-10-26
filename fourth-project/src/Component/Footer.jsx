32d2bac4bef28339a5de33753a5ff8ec676ad752
mnethaji838 <mnethaji838@gmail.com>
2024-10-26 09:16:19 +0530

    nimo-project

--- fourth-project/src/Component/Footer.jsx
+++ fourth-project/src/Component/Footer.jsx
@@ -1,14 +1,17 @@
 import { Component } from "react";
 
-export class Footer extends Component{
-    constructor(){
+export class Footer extends Component {
+    constructor() {
         super()
     }
-    render(){
-        return(
-            <div>
-                
-            </div>
+    render() {
+        return (
+            <section class="container-fluid footer_section ">
+                <p>
+                    &copy; <span id="displayYear"></span> All Rights Reserved. Design by
+                    <a href="https://html.design/">Free Html Templates</a>
+                </p>
+            </section>
         )
     }
 }
