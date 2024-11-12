12cbd10f05fe0f8fe721938459ce5cdddd1d17af
mnethaji838 <mnethaji838@gmail.com>
2024-11-12 10:24:35 +0530

    added-clinic-project

--- clinick-project/src/App.js
+++ clinick-project/src/App.js
@@ -1,32 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-import AboutUs from './Components/About';
-import Appointment from './Components/Appointment';
-import Features from './Components/Feature';
-import Footer from './Components/Footer';
-import Header from './Components/Header';
-import Navbar from './Components/Navbar';
-import Services from './Components/Service';
-import Spinner from './Components/Spinner';
-import Testimonials from './Components/Testimonial';
-import HeaderTopBar from './Components/Topbar';
-import Doctors from './Components/Team';
+// import './App.css';
+import { Getprobutton } from './Components/Getprobutton';
+import { Headerarea } from './Components/Headerarea';
+import { Preloder } from './Components/Preloder';
+import { Pricingtable } from './Components/Pricingtable';
+import { Sliderarea } from './Components/Sliderarea';
+import { Startappoint } from './Components/Startappoint';
+import { Startblogarea } from './Components/Startblogarea';
+import { Startcalltoaction } from './Components/Startcalltoaction';
+import { Startclients } from './Components/Startclients';
+import { Startfeature } from './Components/Startfeature';
+import { Startfooter } from './Components/Startfooterarea';
+import { Startfortpolio } from './Components/Startfortpolio';
+import { Startfunfact } from './Components/Startfunfact';
+import { Startnewletterarea } from './Components/Startnewletterarea';
+import { Startservice } from './Components/Startservice';
+import { Startshudulearea } from './Components/Startshudulearea';
+import { Startwhychoose } from './Components/Startwhychoose';
+
 
 
 function App() {
   return (
     <div className="App">
-    <AboutUs/>
-    <Appointment/>
-    <Features/>
-    <Footer/>
-    <Header/>
-    <Navbar/>
-    <Services/>
-    <Spinner/>
-    <Testimonials/>
-    <HeaderTopBar/>
-    <Doctors/>
+      {/* <Preloder /> */}
+      <Getprobutton/>
+      <Headerarea/>
+      <Sliderarea/>
+      <Startshudulearea/>
+      <Startfeature/>
+      <Startfunfact/>
+      <Startwhychoose/>
+      <Startcalltoaction/>
+      <Startfortpolio/>
+      <Startservice/>
+      <Pricingtable/>
+      <Startblogarea/>
+      <Startclients/>
+      <Startappoint/>
+      <Startnewletterarea/>
+      <Startfooter/>
     </div>
   );
 }
---
+++ clinick-project/src/Components/Startclients.jsx
@@ -0,0 +1,46 @@
+import { Component } from "react";
+
+export class Startclients extends Component {
+    constructor() {
+        super()
+    }
+    render() {
+        return <div class="clients overlay">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-12">
+                        <div class="owl-carousel clients-slider">
+                            <div class="single-clients">
+                                <img src="img/client1.png" alt="#"/>
+                            </div>
+                            <div class="single-clients">
+                                <img src="img/client2.png" alt="#"/>
+                            </div>
+                            <div class="single-clients">
+                                <img src="img/client3.png" alt="#"/>
+                            </div>
+                            <div class="single-clients">
+                                <img src="img/client4.png" alt="#"/>
+                            </div>
+                            <div class="single-clients">
+                                <img src="img/client5.png" alt="#"/>
+                            </div>
+                            <div class="single-clients">
+                                <img src="img/client1.png" alt="#"/>
+                            </div>
+                            <div class="single-clients">
+                                <img src="img/client2.png" alt="#"/>
+                            </div>
+                            <div class="single-clients">
+                                <img src="img/client3.png" alt="#"/>
+                            </div>
+                            <div class="single-clients">
+                                <img src="img/client4.png" alt="#"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    }
+}
