d1e981a28774aaafb0a1ba1e7c4449440306bde7
mnethaji838 <mnethaji838@gmail.com>
2024-11-14 10:07:12 +0530

    added-router

--- Electric-App-project/src/App.js
+++ Electric-App-project/src/App.js
@@ -1,8 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
 
 import { Header } from './Component/Header';
-import { Banner } from './Component/Banner';
 import { About } from './Component/About';
 import { Service } from './Component/Service';
 import { Project } from './Component/Project';
@@ -10,19 +12,26 @@ import { Client } from './Component/Client';
 import { Contact } from './Component/Contact';
 import { Footer } from './Component/Footer';
 import { Copyright } from './Component/Copyright';
+import { Home } from './Component/Home';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Banner/>
-      <About/>
-      <Service/>
-      <Project/>
-      <Client/>
-      <Contact/>
-      <Footer/>
-      <Copyright/>
+      <Header />
+
+      <Routes>
+        <Route path="/Home" element={<Home />} />
+
+        <Route path="/About" element={<About />} />
+        <Route path="/Service" element={<Service />} />
+        <Route path="/Project" element={<Project />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Client" element={<Client />} />
+      </Routes>
+
+
+      <Footer />
+      <Copyright />
     </div>
   );
 }
--- Electric-App-project/src/Component/Header.jsx
+++ Electric-App-project/src/Component/Header.jsx
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import { Link } from 'react-router-dom';
 
 
 export class Header extends Component {
@@ -18,15 +19,51 @@ export class Header extends Component {
                         <div className="col-md-9">
                             <div className="menu_text">
                                 <ul>
-                                    <li><a href="index.html">HOME</a></li>
-                                    <li><a href="about.html">ABOUT</a></li>
-                                    <li><a href="services.html">SERVICES</a></li>
-                                    <li><a href="projects.html">PROJECTS</a></li>
-                                    <li><a href="contact.html">CONTACT US</a></li>
-                                    <li><a href="clients.html">CLIENTS</a></li>
+                                    {/* <a href="index.html">HOME</a> */}
+                                    <li>
+                                        <Link to={'Home'}>
+                                            HOME
+                                        </Link>
+                                    </li>
+
+                                    {/* <a href="About">ABOUT</a> */}
+                                    <li>
+                                        <Link to={'About'}>
+                                            ABOUT
+                                        </Link>
+                                    </li>
+
+                                    {/* <a href="Service">SERVICES</a> */}
+                                    <li>
+                                        <Link to={'Service'}>
+                                            SERVICES
+                                        </Link>
+                                    </li>
+
+                                    {/* <a href="Project">PROJECTS</a> */}
+                                    <li>
+                                        <Link to={'Project'}>
+                                        PROJECTS
+                                        </Link>
+                                    </li>
+
+                                    {/* <a href="Contact">CONTACT US</a> */}
+                                    <li>
+                                        <Link to={'Contact'}>
+                                            CONTACT US
+                                        </Link>
+                                    </li>
+
+                                    {/* <a href="Client">CLIENTS</a> */}
+                                    <li>
+                                        <Link to={'Client'}>
+                                            CLIENTS
+                                        </Link>
+                                    </li>
+
                                     <li><a href="#"><img src="images/search-icon.png" /></a></li>
                                 </ul>
-                                <div id="myNav" className="overlay">
+                                {/* <div id="myNav" className="overlay">
                                     <a href="javascript:void(0)" className="closebtn" onclick="closeNav()">&times;</a>
                                     <div className="overlay-content">
                                         <a href="index.html">HOME</a>
@@ -36,10 +73,10 @@ export class Header extends Component {
                                         <a href="contact.html">CONTACT US</a>
                                         <a href="client.html">CLIENTS</a>
                                     </div>
-                                </div>
+                                </div> */}
                                 <span
-                                //  style="font-size:33px;cursor:pointer; color: #ffffff;" 
-                                 onclick="openNav()"><img src="images/toggle.png" className="toggle_menu" /></span>
+                                    //  style="font-size:33px;cursor:pointer; color: #ffffff;" 
+                                    onclick="openNav()"><img src="images/toggle.png" className="toggle_menu" /></span>
                             </div>
 
 
